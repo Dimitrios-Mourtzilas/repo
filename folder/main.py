188f0dbbbf7f759516e8dd21cfb2c7c75c74bf5a
Dimitrios-Mourtzilas <dimimour@students.kastoria.teiwm.gr>
2021-10-20 11:04:14 +0300

    first commit on the new project

---
+++ folder/main.py
@@ -0,0 +1,12 @@
+
+
+def run():
+    print([i for i in range(0,10)])
+    #Alternative way
+    for i in range(0,10):
+        print(i) # print(f"{i}")
+def main():
+    run()
+
+if __name__ == "__main__":
+    main()
